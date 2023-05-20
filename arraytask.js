ae54fbbf760fe7627edd7a907eff3a99471ba8a3
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-05-20 18:40:17 -0400

    backToFront

--- arraytask.js
+++ arraytask.js
@@ -1,10 +1,10 @@
 let newARRAY = new Array; 
-const pushFont = (arr) =>{ 
+const backToFront = (arr) =>{ 
     temp = arr[arr.length-1]; 
     arr.pop();
     newARRAY.push(temp);
     for(let i= 0;i<arr.length; i++ ){newARRAY.push(arr[i]);};
 }
 let sample1 = new Array (8,5,3,4)
-pushFont(sample1)
+backToFront(sample1)
 console.log(newARRAY)
