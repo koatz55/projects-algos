a860e0a151a0ff8db608ba6331a44c65c38d9fd0
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-05-20 18:44:34 -0400

    push front test 1

--- arraytask.js
+++ arraytask.js
@@ -1,4 +1,5 @@
 let newARRAY = new Array; 
+
 const backToFront = (arr) =>{ 
     temp = arr[arr.length-1]; 
     arr.pop();
@@ -8,3 +9,13 @@ const backToFront = (arr) =>{
 let sample1 = new Array (8,5,3,4)
 backToFront(sample1)
 console.log(newARRAY)
+
+let sample2 = new Array ();
+const pushFront = (arr,num) =>{ 
+    temp = num; 
+    sample2.push(temp);
+    for(let i= 0;i<arr.length; i++ ){sample2.push(arr[i]);};
+}
+
+pushFront(sample1,7)
+console.log(sample2)
